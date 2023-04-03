a640a0e751a82d1caf20034e61c9afeca6702384
Naimul Islam <naimulislam19149@gmail.com>
2023-04-03 21:57:46 +0600

    role added

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,3 +1,5 @@
+import { Role } from "@prisma/client";
+import { prisma } from "src/server/db";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
@@ -18,4 +20,19 @@ export const exampleRouter = createTRPCRouter({
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
   }),
+
+  updateUser: protectedProcedure
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    .input(z.object({ role: z.enum([Role.Employer, Role.Jobseeker]) }))
+    .mutation(async ({ ctx, input }) => {
+      await ctx.prisma.user.update({
+        where: {
+          id: ctx.session.user.id,
+        },
+        data: {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          role: input.role,
+        },
+      });
+    }),
 });
