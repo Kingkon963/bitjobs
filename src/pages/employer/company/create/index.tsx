910c9a6d24864e0fe9e94723c880a14a697a458b
Naimul Islam <naimulislam19149@gmail.com>
2023-04-09 18:15:55 +0600

    company creation done

--- src/pages/employer/company/create/index.tsx
+++ src/pages/employer/company/create/index.tsx
@@ -1,13 +1,40 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import EmployerLayout from "@components/layouts/EmployerLayout";
+import { api } from "@utils/api";
 import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { type SubmitHandler, useForm } from "react-hook-form";
+
+type CompanyCreateForm = {
+  name: string;
+  location: string;
+};
 
 const CompanyCreatePage: NextPage = () => {
   const { data: session } = useSession();
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<CompanyCreateForm>();
+  const createCompanyMutation = api.company.create.useMutation({
+    onSuccess: async () => {
+      await router.push(`/employer/company`);
+    },
+  });
+
+  const onSubmit: SubmitHandler<CompanyCreateForm> = async (data) => {
+    console.log(data);
+    if (!session?.user?.id) return;
+    await createCompanyMutation.mutateAsync({
+      name: data.name,
+      location: data.location,
+      userId: session?.user?.id,
+    });
+  };
+
   return (
     <EmployerLayout title="Company Create Page">
       <div className="max-w-lg">
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="flex flex-col gap-4">
             <div>
               <label
@@ -18,9 +45,8 @@ const CompanyCreatePage: NextPage = () => {
               </label>
               <div className="mt-1">
                 <input
+                  {...register("name")}
                   type="text"
-                  name="companyName"
-                  id="companyName"
                   className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                   placeholder="Company Name"
                 />
@@ -35,9 +61,8 @@ const CompanyCreatePage: NextPage = () => {
               </label>
               <div className="mt-1">
                 <input
+                  {...register("location")}
                   type="text"
-                  name="companyLocation"
-                  id="companyLocation"
                   className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                   placeholder="Company Location"
                 />
@@ -63,7 +88,7 @@ const CompanyCreatePage: NextPage = () => {
               </div>
             </div>
             <button
-              type="button"
+              type="submit"
               className="mr-auto inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
               Save
