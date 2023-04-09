0f52d013a36531ea71ddebec56da0cdf1bb43f88
Naimul Islam <naimulislam19149@gmail.com>
2023-04-09 20:21:04 +0600

    shows company details

--- src/pages/employer/company/index.tsx
+++ src/pages/employer/company/index.tsx
@@ -1,10 +1,27 @@
 import EmployerLayout from "@components/layouts/EmployerLayout";
+import { api } from "@utils/api";
 import { type NextPage } from "next";
+import { useSession } from "next-auth/react";
 
 const CompanyPage: NextPage = () => {
+  const { data: session } = useSession();
+  const getCompanyQuery = api.company.get.useQuery({
+    userId: session?.user?.id || "",
+  });
+
+  const isLoaded = getCompanyQuery.isSuccess;
+  const isError = getCompanyQuery.isError;
+  const company = getCompanyQuery.data;
+
   return (
     <EmployerLayout>
-      <h1>Company Page</h1>
+      {isLoaded && company && (
+        <div>
+          <h1 className="text-4xl">{company.name}</h1>
+          <p className="mt-1 text-xl">{company.location}</p>
+        </div>
+      )}
+      {isError && <p>Something went wrong</p>}
     </EmployerLayout>
   );
 };
