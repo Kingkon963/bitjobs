90c2230fbbde857ae2a6447437d7f4ba539cd491
Naimul Islam <naimulislam19149@gmail.com>
2023-04-23 19:53:26 +0600

    removed unused imports

--- src/components/employer/JobLocationForm.tsx
+++ src/components/employer/JobLocationForm.tsx
@@ -1,13 +1,8 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import AddressFinderForm from "@components/AddressFinderForm";
 import PanelCard from "@components/PanelCard";
-import Stepper, { type StepProps } from "@components/Stepper";
-import EmployerLayout from "@components/layouts/EmployerLayout";
 import { api } from "@utils/api";
-import { type NextPage } from "next";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
 import useCreateJobStore from "~/hooks/stores/useCreateJobStore";
 
 const JobLocationForm = () => {
