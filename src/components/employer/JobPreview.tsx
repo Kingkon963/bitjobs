007c4bc5ca40677c4195780d779b19ff75b75905
Naimul Islam <naimulislam19149@gmail.com>
2023-05-24 21:48:45 +0600

    separate job publish btn

--- src/components/employer/JobPreview.tsx
+++ src/components/employer/JobPreview.tsx
@@ -1,28 +1,14 @@
 import Loading from "@components/Loading";
 import { api } from "@utils/api";
-import { useRouter } from "next/router";
 import React from "react";
 import useCreateJobStore from "~/hooks/stores/useCreateJobStore";
+import JobPublishButton from "./JobPublishButton";
 
 function JobPreview() {
-  const router = useRouter();
   const createdJob = useCreateJobStore();
   const getJobForEditQuery = api.job.getJobForEdit.useQuery(createdJob.jobId, {
     enabled: !!createdJob.jobId,
   });
-  const publishJobMutation = api.job.publishJob.useMutation({
-    onSuccess: async () => {
-      await getJobForEditQuery.refetch();
-      await router.push("/employer");
-    },
-  });
-
-  const handlePublish = () => {
-    if (!getJobForEditQuery.data) return;
-    publishJobMutation.mutate({
-      id: getJobForEditQuery.data.id,
-    });
-  };
 
   return (
     <div>
@@ -58,15 +44,9 @@ function JobPreview() {
                 </p>
               </div>
               <div className="mt-8 flex justify-end">
-                <button
-                  className="inline-flex items-center rounded bg-indigo-500 py-2 px-4 font-bold text-white hover:bg-indigo-700"
-                  onClick={handlePublish}
-                  disabled={publishJobMutation.isLoading}
-                >
-                  {publishJobMutation.isLoading && <Loading color="white" />}
-                  {publishJobMutation.isSuccess && <span>Published</span>}
-                  {!publishJobMutation.isSuccess && <span>Publish</span>}
-                </button>
+                {getJobForEditQuery.data && (
+                  <JobPublishButton jobId={getJobForEditQuery.data.id} />
+                )}
               </div>
             </div>
           )}
---
+++ src/components/employer/JobPublishButton.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { api } from "@utils/api";
+import Loading from "@components/Loading";
+
+function JobPublishButton({ jobId }: { jobId: string }) {
+  const router = useRouter();
+  const publishJobMutation = api.job.publishJob.useMutation({
+    onSuccess: async () => {
+      await router.push("/employer");
+    },
+  });
+
+  const handlePublish = () => {
+    if (!jobId) return;
+    publishJobMutation.mutate({
+      id: jobId,
+    });
+  };
+
+  return (
+    <button
+      className="inline-flex items-center rounded bg-indigo-500 py-2 px-4 font-bold text-white hover:bg-indigo-700"
+      onClick={handlePublish}
+      disabled={publishJobMutation.isLoading}
+    >
+      {publishJobMutation.isLoading && <Loading color="white" />}
+      {publishJobMutation.isSuccess && <span>Published</span>}
+      {!publishJobMutation.isSuccess && <span>Publish</span>}
+    </button>
+  );
+}
+
+export default JobPublishButton;
