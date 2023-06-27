e839e676fc5a81f8d1d6a91a9933bf95a7e9b96f
Naimul Islam <naimulislam19149@gmail.com>
2023-06-27 11:51:54 +0600

    fixed env import error

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -8,7 +8,7 @@ import {
 } from "next-auth";
 import GoogleProvider, { type GoogleProfile } from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { env } from "../env.js/index.js";
+import { env } from "../env.mjs";
 import { prisma } from "./db";
 
 /**
