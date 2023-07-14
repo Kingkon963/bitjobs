dce0dd4eefee151e71c9e3ab2b3b94740f8d091b
Naimul Islam <naimulislam19149@gmail.com>
2023-07-15 01:18:40 +0600

    delete dialog added

--- src/components/jobseeker/profile/WorkExperienceList.tsx
+++ src/components/jobseeker/profile/WorkExperienceList.tsx
@@ -5,16 +5,27 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { AiOutlineArrowUp, AiOutlineArrowDown } from "react-icons/ai";
 
 function WorkExperienceCard() {
   const handleDelete = () => {
-    const confirm = window.confirm("Are you sure you want to delete this?");
-    if (confirm) {
-      console.log("Delete");
-    }
+    // const confirm = window.confirm("Are you sure you want to delete this?");
+    // if (confirm) {
+    //   console.log("Delete");
+    // }
   };
   return (
     <div className="card relative bg-base-100 p-4">
@@ -33,31 +44,47 @@ function WorkExperienceCard() {
         I&apos;ve used React and TailwindCSS. I&apos;ve also used Storybook to
         build UI components.
       </p>
-      <DropdownMenu>
-        <div className="absolute right-5 top-5 flex gap-1">
-          <button>
-            <AiOutlineArrowDown />
-          </button>
-          <button>
-            <AiOutlineArrowUp />
-          </button>
-          <DropdownMenuTrigger>
-            <BsThreeDotsVertical />
-          </DropdownMenuTrigger>
-        </div>
-        <DropdownMenuContent className="flex flex-col gap-1 py-2">
-          {/* <DropdownMenuLabel>Actions</DropdownMenuLabel> */}
-          <DropdownMenuItem className="text-md hover:cursor-pointer hover:bg-base-200 focus:bg-base-200 active:bg-base-300">
-            Edit
-          </DropdownMenuItem>
-          <DropdownMenuItem
-            className="text-md text-error hover:cursor-pointer hover:bg-base-200 focus:bg-base-200 active:bg-base-300"
-            onClick={handleDelete}
-          >
-            Delete
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <AlertDialog>
+        <DropdownMenu>
+          <div className="absolute right-5 top-5 flex gap-1">
+            <button>
+              <AiOutlineArrowDown />
+            </button>
+            <button>
+              <AiOutlineArrowUp />
+            </button>
+            <DropdownMenuTrigger>
+              <BsThreeDotsVertical />
+            </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent className="flex flex-col gap-1 py-2">
+            {/* <DropdownMenuLabel>Actions</DropdownMenuLabel> */}
+            <DropdownMenuItem className="text-md hover:cursor-pointer hover:bg-base-200 focus:bg-base-200 active:bg-base-300">
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="text-md text-error hover:cursor-pointer hover:bg-base-200 focus:bg-base-200 active:bg-base-300"
+              // onClick={handleDelete}
+              asChild
+            >
+              <AlertDialogTrigger>Delete</AlertDialogTrigger>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              work experience data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction className="bg-error hover:bg-error">Delete</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
--- src/pages/jobseeker/profile/index.tsx
+++ src/pages/jobseeker/profile/index.tsx
@@ -4,79 +4,81 @@ import WorkExperienceList from "@components/jobseeker/profile/WorkExperienceList
 import JobseekerLayout from "@components/layouts/JobseekerLayout";
 import { type NextPage } from "next";
 import { RxAvatar } from "react-icons/rx";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
 
 const JobseekerProfilePage: NextPage = () => {
   return (
     <>
-    <JobseekerLayout>
-      <div className="flex gap-2">
-        {/* Left panel */}
-        <div className="max-w-xs flex-1 border p-2">
-          <div className="flex flex-col items-center justify-center">
-            <div className="avatar">
-              <div className="w-24 rounded-full">
-                <RxAvatar className="h-full w-full" />
+      <JobseekerLayout>
+        <div className="flex gap-2">
+          {/* Left panel */}
+          <div className="max-w-xs flex-1 border p-2">
+            <div className="flex flex-col items-center justify-center">
+              <div className="avatar">
+                <div className="w-24 rounded-full">
+                  <RxAvatar className="h-full w-full" />
+                </div>
               </div>
+              <h3 className="text-xl">Name</h3>
             </div>
-            <h3 className="text-xl">Name</h3>
-            <Dialog>
-  <DialogTrigger>Open</DialogTrigger>
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Are you sure absolutely sure?</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
-</Dialog>
           </div>
-        </div>
 
-        {/* Right panel */}
-        <div className="flex-1">
-          <div className="w-full flex flex-col gap-4 border p-2">
-            <div className="collapse bg-base-200 overflow-visible">
-              <input type="radio" name="profile-info-accordion" id="profile-info-accordion-1" defaultChecked/>
-              <label htmlFor="profile-info-accordion-1" className="collapse-title text-xl font-medium">
-                Personal Information
-              </label>
-              <div className="collapse-content">
-                <PersonalInfo />
+          {/* Right panel */}
+          <div className="flex-1">
+            <div className="flex w-full flex-col gap-4 border p-2">
+              <div className="collapse overflow-visible bg-base-200">
+                <input
+                  type="radio"
+                  name="profile-info-accordion"
+                  id="profile-info-accordion-1"
+                  defaultChecked
+                />
+                <label
+                  htmlFor="profile-info-accordion-1"
+                  className="collapse-title text-xl font-medium"
+                >
+                  Personal Information
+                </label>
+                <div className="collapse-content">
+                  <PersonalInfo />
+                </div>
               </div>
-            </div>
-            <div className="collapse bg-base-200">
-              <input type="radio" name="profile-info-accordion" id="profile-info-accordion-2"/>
-              <label htmlFor="profile-info-accordion-2" className="collapse-title text-xl font-medium">
-                Work Experience
-              </label>
-              <div className="collapse-content">
-                <WorkExperienceList />
+              <div className="collapse bg-base-200">
+                <input
+                  type="radio"
+                  name="profile-info-accordion"
+                  id="profile-info-accordion-2"
+                />
+                <label
+                  htmlFor="profile-info-accordion-2"
+                  className="collapse-title text-xl font-medium"
+                >
+                  Work Experience
+                </label>
+                <div className="collapse-content">
+                  <WorkExperienceList />
+                </div>
               </div>
-            </div>
-            <div className="collapse bg-base-200">
-              <input type="radio" name="profile-info-accordion" id="profile-info-accordion-3"/>
-              <label htmlFor="profile-info-accordion-3" className="collapse-title text-xl font-medium">
-                Upload CV/Resume
-              </label>
-              <div className="collapse-content">
-                <UploadCV />
+              <div className="collapse bg-base-200">
+                <input
+                  type="radio"
+                  name="profile-info-accordion"
+                  id="profile-info-accordion-3"
+                />
+                <label
+                  htmlFor="profile-info-accordion-3"
+                  className="collapse-title text-xl font-medium"
+                >
+                  Upload CV/Resume
+                </label>
+                <div className="collapse-content">
+                  <UploadCV />
+                </div>
               </div>
             </div>
           </div>
+          {/* <div className="max-w-xs flex-1 border p-2">c</div> */}
         </div>
-        {/* <div className="max-w-xs flex-1 border p-2">c</div> */}
-      </div>
-    </JobseekerLayout>
+      </JobseekerLayout>
     </>
   );
 };
