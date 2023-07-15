680391813db1bb2ab21a275301698dd20e4c826e
Naimul Islam <naimulislam19149@gmail.com>
2023-07-15 12:43:16 +0600

    website and linkedin logo added

--- src/components/jobseeker/profile/EditWorkExperienceDialog.tsx
+++ src/components/jobseeker/profile/EditWorkExperienceDialog.tsx
@@ -7,6 +7,29 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, {
+      message: "Username must be at least 2 characters long",
+    })
+    .max(50),
+});
 
 type EditWorkExperienceDialogProps = {
   // can pass only DialogTrigger
@@ -14,32 +37,63 @@ type EditWorkExperienceDialogProps = {
 };
 
 function EditWorkExperienceDialog({ children }: EditWorkExperienceDialogProps) {
-  
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
   useEffect(() => {
     React.Children.forEach(children, (child) => {
       // console.log(child);
       if (React.isValidElement(child)) {
-        if(child.type !== DialogTrigger) {
+        if (child.type !== DialogTrigger) {
           throw new Error(
             "EditWorkExperienceDialog component's children must be DialogTrigger"
           );
         }
       }
-    }
-    );
+    });
   }, [children]);
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  }
 
   return (
     <Dialog>
       {children}
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+          <DialogTitle>Work Experience</DialogTitle>
           <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            Add details about your work experience
           </DialogDescription>
         </DialogHeader>
+        <div className="grid grid-cols-1">
+        <Form {...form}>
+      <form onSubmit={e => void form.handleSubmit(onSubmit)(e)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </Form>
+        </div>
       </DialogContent>
     </Dialog>
   );
